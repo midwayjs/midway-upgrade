7b238216b38482ca375fdd88881b4ec1de5b4946
echosoar <82163514@qq.com>
2022-05-26 15:43:07 +0800

    test: upgrade midway decorator

--- test/index.test.ts
+++ test/index.test.ts
@@ -21,11 +21,15 @@ describe('index.test.ts', () => {
   it('web 2 to 3', async () => {
     const baseDir = resolve(__dirname, './fixtures/web-v2');
     const target = resolve(__dirname, './fixtures/tmp/web-v2');
+    const lockFile = resolve(__dirname, './fixtures/tmp/web-v2/package-lock.json');
     if (existsSync(target)) {
       await remove(target);
     }
     await copy(baseDir, target);
     await upgrade(target);
+    if (existsSync(lockFile)) {
+      await remove(lockFile);
+    }
     const { close, port } = await run(target, { port: '12331' });
     const now = `${Date.now()}`;
     const res = await axios.get(`http://127.0.0.1:${port}/?name=${now}`);
